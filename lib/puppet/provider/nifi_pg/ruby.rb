4e03823ddbd73f6a1ec3eb958677678136c1484b
Israel Calvete <icalvete@gmail.com>
2017-07-18 16:26:31 +0200

    Style issue

--- lib/puppet/provider/nifi_pg/ruby.rb
+++ lib/puppet/provider/nifi_pg/ruby.rb
@@ -3,104 +3,104 @@ require 'nifi_sdk_ruby'
 Puppet::Type.type(:nifi_pg).provide(:ruby) do
 
     def init
-      schema = 'http'
-      port = 8080
-      cert = nil
-      cert_key = nil
+      schema        = 'http'
+      port          = 8080
+      cert          = nil
+      cert_key      = nil
       cert_password = nil
 
       if :secure
-        schema = 'https'
-        port = 9443
-        cert = resource[:cert]
-        cert_key = resource[:cert_key]
+        schema        = 'https'
+        port          = 9443
+        cert          = resource[:cert]
+        cert_key      = resource[:cert_key]
         cert_password = resource[:cert_password]
       end
 
       nifi_client = Nifi.new(
-        :schema => schema,
-        :port => port,
-        :cert => cert.to_s,
-        :cert_key => cert_key.to_s,
-        :cert_password=> cert_password.to_s
+        :schema        => schema,
+        :port          => port,
+        :cert          => cert.to_s,
+        :cert_key      => cert_key.to_s,
+        :cert_password => cert_password.to_s
       )
       nifi_client.set_debug true
     end
 
     def create
-      schema = 'http'
-      port = 8080
-      cert = nil
-      cert_key = nil
+      schema        = 'http'
+      port          = 8080
+      cert          = nil
+      cert_key      = nil
       cert_password = nil
 
       if :secure
-        schema = 'https'
-        port = 9443
-        cert = resource[:cert]
-        cert_key = resource[:cert_key]
+        schema        = 'https'
+        port          = 9443
+        cert          = resource[:cert]
+        cert_key      = resource[:cert_key]
         cert_password = resource[:cert_password]
       end
 
       nifi_client = Nifi.new(
-        :schema => schema,
-        :port => port,
-        :cert => cert.to_s,
-        :cert_key => cert_key.to_s,
-        :cert_password=> cert_password.to_s
+        :schema        => schema,
+        :port          => port,
+        :cert          => cert.to_s,
+        :cert_key      => cert_key.to_s,
+        :cert_password => cert_password.to_s
       )
       nifi_client.set_debug true
       nifi_client.create_process_group(:name => resource[:name])
     end
 
     def destroy
-      schema = 'http'
-      port = 8080
-      cert = nil
-      cert_key = nil
+      schema        = 'http'
+      port          = 8080
+      cert          = nil
+      cert_key      = nil
       cert_password = nil
 
       if :secure
-        schema = 'https'
-        port = 9443
-        cert = resource[:cert]
-        cert_key = resource[:cert_key]
+        schema        = 'https'
+        port          = 9443
+        cert          = resource[:cert]
+        cert_key      = resource[:cert_key]
         cert_password = resource[:cert_password]
       end
 
       nifi_client = Nifi.new(
-        :schema => schema,
-        :port => port,
-        :cert => cert.to_s,
-        :cert_key => cert_key.to_s,
-        :cert_password=> cert_password.to_s
-      )
+        :schema        => schema,
+        :port          => port,
+        :cert          => cert.to_s,
+        :cert_key      => cert_key.to_s,
+        :cert_password => cert_password.to_s
+      ))
       nifi_client.set_debug true
       pg = nifi_client.get_process_group_by_name resource[:name]
       nifi_client.delete_process_group pg['id']
     end
 
     def exists?
-      schema = 'http'
-      port = 8080
-      cert = nil
-      cert_key = nil
+      schema        = 'http'
+      port          = 8080
+      cert          = nil
+      cert_key      = nil
       cert_password = nil
 
       if :secure
-        schema = 'https'
-        port = 9443
-        cert = resource[:cert]
-        cert_key = resource[:cert_key]
+        schema        = 'https'
+        port          = 9443
+        cert          = resource[:cert]
+        cert_key      = resource[:cert_key]
         cert_password = resource[:cert_password]
       end
 
       nifi_client = Nifi.new(
-        :schema => schema,
-        :port => port,
-        :cert => cert.to_s,
-        :cert_key => cert_key.to_s,
-        :cert_password=> cert_password.to_s
+        :schema        => schema,
+        :port          => port,
+        :cert          => cert.to_s,
+        :cert_key      => cert_key.to_s,
+        :cert_password => cert_password.to_s
       )
       nifi_client.set_debug true
       nifi_client.process_group_by_name? resource[:name]
--- lib/puppet/provider/nifi_template/ruby.rb
+++ lib/puppet/provider/nifi_template/ruby.rb
@@ -3,77 +3,77 @@ require 'nifi_sdk_ruby'
 Puppet::Type.type(:nifi_template).provide(:ruby) do
 
     def init
-      schema = 'http'
-      port = 8080
-      cert = nil
-      cert_key = nil
+      schema        = 'http'
+      port          = 8080
+      cert          = nil
+      cert_key      = nil
       cert_password = nil
 
       if :secure
-        schema = 'https'
-        port = 9443
-        cert = resource[:cert]
-        cert_key = resource[:cert_key]
+        schema        = 'https'
+        port          = 9443
+        cert          = resource[:cert]
+        cert_key      = resource[:cert_key]
         cert_password = resource[:cert_password]
       end
 
       nifi_client = Nifi.new(
-        :schema => schema,
-        :port => port,
-        :cert => cert.to_s,
-        :cert_key => cert_key.to_s,
-        :cert_password=> cert_password.to_s
+        :schema        => schema,
+        :port          => port,
+        :cert          => cert.to_s,
+        :cert_key      => cert_key.to_s,
+        :cert_password => cert_password.to_s
       )
       nifi_client.set_debug true
     end
 
     def create
-      schema = 'http'
-      port = 8080
-      cert = nil
-      cert_key = nil
+      schema        = 'http'
+      port          = 8080
+      cert          = nil
+      cert_key      = nil
       cert_password = nil
 
       if :secure
-        schema = 'https'
-        port = 9443
-        cert = resource[:cert]
-        cert_key = resource[:cert_key]
+        schema        = 'https'
+        port          = 9443
+        cert          = resource[:cert]
+        cert_key      = resource[:cert_key]
         cert_password = resource[:cert_password]
       end
 
       nifi_client = Nifi.new(
-        :schema => schema,
-        :port => port,
-        :cert => cert.to_s,
-        :cert_key => cert_key.to_s,
-        :cert_password=> cert_password.to_s
+        :schema        => schema,
+        :port          => port,
+        :cert          => cert.to_s,
+        :cert_key      => cert_key.to_s,
+        :cert_password => cert_password.to_s
       )
       nifi_client.set_debug true
       nifi_client.upload_template(:path => resource[:path])
     end
 
     def destroy
-      schema = 'http'
-      port = 8080
-      cert = nil
-      cert_key = nil
+      schema        = 'http'
+      port          = 8080
+      cert          = nil
+      cert_key      = nil
       cert_password = nil
 
       if :secure
-        schema = 'https'
-        port = 9443
-        cert = resource[:cert]
-        cert_key = resource[:cert_key]
+        schema        = 'https'
+        port          = 9443
+        cert          = resource[:cert]
+        cert_key      = resource[:cert_key]
         cert_password = resource[:cert_password]
       end
 
       nifi_client = Nifi.new(
-        :schema => schema,
-        :port => port,
-        :cert => cert.to_s,
-        :cert_key => cert_key.to_s,
-        :cert_password=> cert_password.to_s
+        :schema        => schema,
+        :port          => port,
+        :cert          => cert.to_s,
+        :cert_key      => cert_key.to_s,
+        :cert_password => cert_password.to_s
       )
       nifi_client.set_debug true
       t_id = nifi_client.get_template_by_name resource[:name]
@@ -81,26 +81,26 @@ Puppet::Type.type(:nifi_template).provide(:ruby) do
     end
 
     def exists?
-      schema = 'http'
-      port = 8080
-      cert = nil
-      cert_key = nil
+      schema        = 'http'
+      port          = 8080
+      cert          = nil
+      cert_key      = nil
       cert_password = nil
 
       if :secure
-        schema = 'https'
-        port = 9443
-        cert = resource[:cert]
-        cert_key = resource[:cert_key]
+        schema        = 'https'
+        port          = 9443
+        cert          = resource[:cert]
+        cert_key      = resource[:cert_key]
         cert_password = resource[:cert_password]
       end
 
       nifi_client = Nifi.new(
-        :schema => schema,
-        :port => port,
-        :cert => cert.to_s,
-        :cert_key => cert_key.to_s,
-        :cert_password=> cert_password.to_s
+        :schema        => schema,
+        :port          => port,
+        :cert          => cert.to_s,
+        :cert_key      => cert_key.to_s,
+        :cert_password => cert_password.to_s
       )
       nifi_client.set_debug true
       nifi_client.template_by_name? resource[:name]
