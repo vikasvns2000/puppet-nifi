ca312b7e346f48befeb4e055663b85840213d095
vikasvns2000 <41138850+vikasvns2000@users.noreply.github.com>
2018-12-27 15:49:04 +0530

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,7 +8,7 @@ class nifi::params {
     }
   }
 
-  $version               = '1.5.0'
+  $version               = '1.6.0'
   $root_path             = '/opt'
   $repo_scheme           = 'http'
   $repo_domain           = 'archive.apache.org'
